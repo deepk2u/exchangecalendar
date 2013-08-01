ab10350a75ec10e39960a48f37e59d727c085e52
egjlmqr <deepak.z.kumar@ericsson.com>
2013-08-01 17:09:46 +0530

    added code to show optional and required attendees on calendar event dialog

--- chrome/content/calendar-event-dialog.js
+++ chrome/content/calendar-event-dialog.js
@@ -35,94 +35,277 @@ var Cc = Components.classes;
 Cu.import("resource://exchangecalendar/ecFunctions.js");
 Cu.import("resource://calendar/modules/calUtils.jsm");
 
-if (! exchWebService) var exchWebService = {};
+if (!exchWebService) var exchWebService = {};
 
 exchWebService.eventDialog = {
 
-	_initialized: false,
-	onLoad: function _onLoad()
-	{
-		if (this._initialized) return;
+    _initialized: false,
+    onLoad: function _onLoad() {
+        if (this._initialized) return;
 
-exchWebService.commonFunctions.LOG(" !! exchWebService.eventDialog.onLoad 1");
+        exchWebService.commonFunctions.LOG(" !! exchWebService.eventDialog.onLoad 1");
+        // document.getElementById("event-grid-attendee-row").setAttribute("collapsed" , true);
+        exchWebService.eventDialog.updateAttendees();
+        if (document.getElementById("todo-entrydate")) {
+            this._initialized = true;
+            // nuke the onload, or we get called every time there's
+            // any load that occurs
+            exchWebService.commonFunctions.LOG(" !! exchWebService.eventDialog.onLoad 1a");
+            window.removeEventListener("load", exchWebService.eventDialog.onLoad, false);
 
-		if (document.getElementById("todo-entrydate")) {
-			this._initialized = true;
-			// nuke the onload, or we get called every time there's
-			// any load that occurs
-exchWebService.commonFunctions.LOG(" !! exchWebService.eventDialog.onLoad 1a");
-			window.removeEventListener("load", exchWebService.eventDialog.onLoad, false);
+            var args = window.arguments[0];
+            var item = args.calendarEvent;
+            if ((!cal.isEvent(item)) && (item.calendar.type == "exchangecalendar")) {
 
-			var args = window.arguments[0];
-			var item = args.calendarEvent;
-			if ((!cal.isEvent(item)) && (item.calendar.type == "exchangecalendar")) {
+                exchWebService.commonFunctions.LOG(" !! exchWebService.eventDialog.onLoad 2");
+                var tmpDatePicker = document.createElement("datepicker");
+                tmpDatePicker.setAttribute("type", "popup");
+                tmpDatePicker.setAttribute("id", "todo-entrydate");
+                tmpDatePicker.setAttribute("value", document.getElementById("todo-entrydate").value);
+                //tmpDatePicker.setAttribute("onchange","dateTimeControls2State(true);exchWebService.eventDialog.updateTime();");
+                tmpDatePicker.addEventListener("change", exchWebService.eventDialog.updateTime, false);
+                if (!document.getElementById("todo-has-entrydate").checked) {
+                    tmpDatePicker.setAttribute("disabled", "true");
+                }
+                document.getElementById("event-grid-startdate-picker-box").replaceChild(tmpDatePicker, document.getElementById("todo-entrydate"));
 
-exchWebService.commonFunctions.LOG(" !! exchWebService.eventDialog.onLoad 2");
-				var tmpDatePicker = document.createElement("datepicker");
-				tmpDatePicker.setAttribute("type","popup");
-				tmpDatePicker.setAttribute("id","todo-entrydate");
-				tmpDatePicker.setAttribute("value",document.getElementById("todo-entrydate").value);
-				//tmpDatePicker.setAttribute("onchange","dateTimeControls2State(true);exchWebService.eventDialog.updateTime();");
-				tmpDatePicker.addEventListener("change", exchWebService.eventDialog.updateTime, false);
-				if (!document.getElementById("todo-has-entrydate").checked) {
-					tmpDatePicker.setAttribute("disabled","true");
-				}
-				document.getElementById("event-grid-startdate-picker-box").replaceChild(tmpDatePicker, document.getElementById("todo-entrydate"));
+                exchWebService.commonFunctions.LOG(" !! exchWebService.eventDialog.onLoad 3");
+                var tmpDatePicker = document.createElement("datepicker");
+                tmpDatePicker.setAttribute("type", "popup");
+                tmpDatePicker.setAttribute("id", "todo-duedate");
+                tmpDatePicker.setAttribute("value", document.getElementById("todo-duedate").value);
+                //tmpDatePicker.setAttribute("onchange","dateTimeControls2State(false);exchWebService.eventDialog.updateTime();");
+                tmpDatePicker.addEventListener("change", exchWebService.eventDialog.updateTime, false);
+                if (!document.getElementById("todo-has-duedate").checked) {
+                    tmpDatePicker.setAttribute("disabled", "true");
+                }
+                document.getElementById("event-grid-enddate-picker-box").replaceChild(tmpDatePicker, document.getElementById("todo-duedate"));
+                exchWebService.commonFunctions.LOG(" !! exchWebService.eventDialog.onLoad 4");
 
-exchWebService.commonFunctions.LOG(" !! exchWebService.eventDialog.onLoad 3");
-				var tmpDatePicker = document.createElement("datepicker");
-				tmpDatePicker.setAttribute("type","popup");
-				tmpDatePicker.setAttribute("id","todo-duedate");
-				tmpDatePicker.setAttribute("value",document.getElementById("todo-duedate").value);
-				//tmpDatePicker.setAttribute("onchange","dateTimeControls2State(false);exchWebService.eventDialog.updateTime();");
-				tmpDatePicker.addEventListener("change", exchWebService.eventDialog.updateTime, false);
-				if (!document.getElementById("todo-has-duedate").checked) {
-					tmpDatePicker.setAttribute("disabled","true");
-				}
-				document.getElementById("event-grid-enddate-picker-box").replaceChild(tmpDatePicker, document.getElementById("todo-duedate"));
-exchWebService.commonFunctions.LOG(" !! exchWebService.eventDialog.onLoad 4");
+                document.getElementById("link-image-top").hidden = true;
+                document.getElementById("link-image-bottom").hidden = true;
+                document.getElementById("keepduration-button").hidden = true;
+                document.getElementById("timezone-starttime").hidden = true;
+                document.getElementById("timezone-endtime").hidden = true;
 
-				document.getElementById("link-image-top").hidden = true;
-				document.getElementById("link-image-bottom").hidden = true;
-				document.getElementById("keepduration-button").hidden = true;
-				document.getElementById("timezone-starttime").hidden = true;
-				document.getElementById("timezone-endtime").hidden = true;
+                if (document.getElementById("item-repeat")) {
+                    document.getElementById("item-repeat").addEventListener("command", exchWebService.eventDialog.updateRepeat, false);
+                }
+                exchWebService.eventDialog.updateTime();
+                exchWebService.eventDialog.updateRepeat();
+            }
+        }
+        exchWebService.commonFunctions.LOG(" !! exchWebService.eventDialog.onLoad 5");
+    },
 
-				if (document.getElementById("item-repeat")) {
-					document.getElementById("item-repeat").addEventListener("command", exchWebService.eventDialog.updateRepeat, false);
-				}
-				exchWebService.eventDialog.updateTime();
-				exchWebService.eventDialog.updateRepeat();
-			}
-		}
-exchWebService.commonFunctions.LOG(" !! exchWebService.eventDialog.onLoad 5");
-	},
+    updateTime: function _updateTime() {
+        exchWebService.commonFunctions.LOG(" ===++ calendar-event-dialog.js");
+        if (document.getElementById("todo-entrydate").dateValue) {
+            document.getElementById("todo-entrydate").dateValue.setHours(12);
+        }
+        if (document.getElementById("todo-duedate").dateValue) {
+            document.getElementById("todo-duedate").dateValue.setHours(13);
+        }
+    },
 
-	updateTime: function _updateTime()
-	{
-		exchWebService.commonFunctions.LOG(" ===++ calendar-event-dialog.js");
-		if (document.getElementById("todo-entrydate").dateValue) {
-			document.getElementById("todo-entrydate").dateValue.setHours(12);
-		}
-		if (document.getElementById("todo-duedate").dateValue) {
-			document.getElementById("todo-duedate").dateValue.setHours(13);
-		}
-	},
+    // This will remove the time value from the repeat part and tooltip.
+    updateRepeat: function _updateRepeat() {
+        var repeatDetails = document.getElementById("repeat-details").childNodes;
+        if (repeatDetails.length == 3) {
+            document.getElementById("repeat-details").removeChild(repeatDetails[2]);
+            var toolTip = repeatDetails[0].getAttribute("tooltiptext");
+            var tmpArray = toolTip.split("\n");
+            tmpArray.splice(2, 1);
+            repeatDetails[0].setAttribute("tooltiptext", tmpArray.join("\n"));
+            repeatDetails[1].setAttribute("tooltiptext", tmpArray.join("\n"));
+        }
+    },
 
-	// This will remove the time value from the repeat part and tooltip.
-	updateRepeat: function _updateRepeat()
-	{
-		var repeatDetails = document.getElementById("repeat-details").childNodes;
-		if (repeatDetails.length == 3) {
-			document.getElementById("repeat-details").removeChild(repeatDetails[2]);
-			var toolTip = repeatDetails[0].getAttribute("tooltiptext");
-			var tmpArray = toolTip.split("\n");
-			tmpArray.splice(2,1);
-			repeatDetails[0].setAttribute("tooltiptext", tmpArray.join("\n"));
-			repeatDetails[1].setAttribute("tooltiptext", tmpArray.join("\n"));
-		}
-	}
+    updateAttendees: function _updateAttendees() {
+        let attendeeRow = document.getElementById("event-grid-attendee-row");
+        attendeeRow.setAttribute('collapsed', 'true');
+        let attendeeRow2 = document.getElementById("event-grid-attendee-row-2");
+        let optAttendeeRow = document.getElementById("event-grid-attendee-row-4");
+        let reqAttendeeRow = document.getElementById("event-grid-attendee-row-3");
+        if (window.attendees && window.attendees.length > 0) {
+            if (isEvent(window.calendarItem)) { // sending email invitations currently only supported for events
+                attendeeRow2.removeAttribute('collapsed');
+            } else {
+                attendeeRow2.setAttribute('collapsed', 'true');
+            }
 
+            let attendeeNames = [];
+            let attendeeEmails = [];
+            let reqAttendeeNames = [];
+            let reqAttendeeEmails = [];
+            let optAttendeeNames = [];
+            let optAttendeeEmails = [];
+            let numAttendees = window.attendees.length;
+            let emailRE = new RegExp("^mailto:(.*)", "i");
+            for (let i = 0; i < numAttendees; i++) {
+                let attendee = window.attendees[i];
+                let name = attendee.commonName;
+                if (attendee.role == "OPT-PARTICIPANT") {
+                    if (name && name.length) {
+                        optAttendeeNames.push(name);
+                        let email = attendee.id;
+                        if (email && email.length) {
+                            if (emailRE.test(email)) {
+                                name += ' <' + RegExp.$1 + '>';
+                            } else {
+                                name += ' <' + email + '>';
+                            }
+                            optAttendeeEmails.push(name);
+                        }
+                    } else if (attendee.id && attendee.id.length) {
+                        let email = attendee.id;
+                        if (emailRE.test(email)) {
+                            optAttendeeNames.push(RegExp.$1);
+                        } else {
+                            optAttendeeNames.push(email);
+                        }
+                    } else {
+                        continue;
+                    }
+
+                } else {
+
+                    if (name && name.length) {
+                        reqAttendeeNames.push(name);
+                        let email = attendee.id;
+                        if (email && email.length) {
+                            if (emailRE.test(email)) {
+                                name += ' <' + RegExp.$1 + '>';
+                            } else {
+                                name += ' <' + email + '>';
+                            }
+                            reqAttendeeEmails.push(name);
+                        }
+                    } else if (attendee.id && attendee.id.length) {
+                        let email = attendee.id;
+                        if (emailRE.test(email)) {
+                            reqAttendeeNames.push(RegExp.$1);
+                        } else {
+                            reqAttendeeNames.push(email);
+                        }
+                    } else {
+                        continue;
+                    }
+
+                }
+            }
+            if (reqAttendeeNames.length > 0) {
+                reqAttendeeRow.removeAttribute('collapsed');
+            } else {
+                reqAttendeeRow.setAttribute('collapsed', 'true');
+            }
+            if (optAttendeeNames.length > 0) {
+                optAttendeeRow.removeAttribute('collapsed');
+            } else {
+                optAttendeeRow.setAttribute('collapsed', 'true');
+            }
+
+            let attendeeList = document.getElementById("attendee-list");
+            let reqAttendeeList = document.getElementById("req-attendee-list-3");
+            let optAttendeeList = document.getElementById("opt-attendee-list-4");
+
+            let callback = function func() {
+                reqAttendeeList.setAttribute('value', reqAttendeeNames.join(', '));
+                reqAttendeeList.setAttribute('tooltiptext', reqAttendeeEmails.join(', '));
+                optAttendeeList.setAttribute('value', optAttendeeNames.join(', '));
+                optAttendeeList.setAttribute('tooltiptext', optAttendeeEmails.join(', '));
+            };
+            setTimeout(callback, 1);
+        } else {
+
+            attendeeRow2.setAttribute('collapsed', 'true');
+            optAttendeeRow.setAttribute('collapsed', 'true');
+            reqAttendeeRow.setAttribute('collapsed', 'true');
+        }
+    },
+
+    editAttendees: function _editAttendees() {
+        let savedWindow = window;
+        let calendar = getCurrentCalendar();
+
+        var callback = function (attendees, organizer, startTime, endTime) {
+            savedWindow.attendees = attendees;
+            if (organizer) {
+                // In case we didn't have an organizer object before we
+                // added attendees to our event we take the one created
+                // by the 'invite attendee'-dialog.
+                if (savedWindow.organizer) {
+                    // The other case is that we already had an organizer object
+                    // before we went throught the 'invite attendee'-dialog. In that
+                    // case make sure we don't carry over attributes that have been
+                    // set to their default values by the dialog but don't actually
+                    // exist in the original organizer object.
+                    if (!savedWindow.organizer.id) {
+                        organizer.id = null;
+                    }
+                    if (!savedWindow.organizer.role) {
+                        organizer.role = null;
+                    }
+                    if (!savedWindow.organizer.participationStatus) {
+                        organizer.participationStatus = null;
+                    }
+                    if (!savedWindow.organizer.commonName) {
+                        organizer.commonName = null;
+                    }
+                }
+                savedWindow.organizer = organizer;
+            }
+            var duration = endTime.subtractDate(startTime);
+            startTime = startTime.clone();
+            endTime = endTime.clone();
+            var kDefaultTimezone = calendarDefaultTimezone();
+            gStartTimezone = startTime.timezone;
+            gEndTimezone = endTime.timezone;
+            gStartTime = startTime.getInTimezone(kDefaultTimezone);
+            gEndTime = endTime.getInTimezone(kDefaultTimezone);
+            gItemDuration = duration;
+            exchWebService.eventDialog.updateAttendees();
+            updateDateTime();
+            updateAllDay();
+            if (isAllDay != gStartTime.isDate) {
+                setShowTimeAs(gStartTime.isDate)
+            }
+        };
+
+        var startTime = gStartTime.getInTimezone(gStartTimezone);
+        var endTime = gEndTime.getInTimezone(gEndTimezone);
+
+        var isAllDay = getElementValue("event-all-day", "checked");
+        if (isAllDay) {
+            startTime.isDate = true;
+            endTime.isDate = true;
+            endTime.day += 1;
+        } else {
+            startTime.isDate = false;
+            endTime.isDate = false;
+        }
+
+        var menuItem = document.getElementById('options-timezone-menuitem');
+        var displayTimezone = menuItem.getAttribute('checked') == 'true';
+
+        var args = new Object();
+        args.startTime = startTime;
+        args.endTime = endTime;
+        args.displayTimezone = displayTimezone;
+        args.attendees = window.attendees;
+        args.organizer = window.organizer && window.organizer.clone();
+        args.calendar = calendar;
+        args.item = window.calendarItem;
+        args.onOk = callback;
+        args.fbWrapper = window.fbWrapper;
+
+        // open the dialog modally
+        openDialog(
+            "chrome://calendar/content/calendar-event-dialog-attendees.xul",
+            "_blank",
+            "chrome,titlebar,modal,resizable",
+            args);
+    }
 }
 
 window.addEventListener("load", exchWebService.eventDialog.onLoad, false);
